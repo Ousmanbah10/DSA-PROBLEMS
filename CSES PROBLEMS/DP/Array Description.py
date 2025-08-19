679785c3fd154b3c0450245cb5494d3c60bdac8d
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-08-18 23:15:03 -0700

    Array Description changes

--- CSES PROBLEMS/DP/Array Description.py
+++ CSES PROBLEMS/DP/Array Description.py
@@ -1,35 +1,30 @@
 
 
-
+MOD = 10**9 + 7
 n , m = list(map(int,input().split()))
 
 print(n,m)
 
 vals = list(map(int,input().split()))
 
-dp = [[0] * (m+2) for _ in range(n+1)]
+dp = [[0] * (m+2) for _ in range(n)]
 
 if vals[0] != 0:
     dp[0][vals[0]] = 1
+
 else:
-    dp[0][1] = 1
+    for v in range(1, m+1):
+        dp[0][v] = 1
     
 for i in range(1,n):
     
-    
-    
-    if vals[i] != 0: # value is not 0 
-        dp[i][vals[i]] += dp[i-1][vals[i]-1]
-        dp[i][vals[i]] += dp[i-1][vals[i]]
-        dp[i][vals[i]] += dp[i-1][vals[i]+1]
-        
-    print("hh")
-    
-    for j in range(1,len(dp[0])-1):
-        
-        if dp[i-1][j] > 0:
-            dp[i][j-1] += 1 # prev
-            dp[i][j] += 1 #current
-            dp[i][j+1] += 1 # next
+    if vals[i] != 0 : # value is not 0 
+        dp[i][vals[i]] = (dp[i-1][vals[i]-1] + dp[i-1][vals[i]] + dp[i-1][vals[i]+1]) % MOD
+    else:
+        for j in range(1,len(dp[0])-1):
+            
+            
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j] + dp[i-1][j+1]) % MOD
+ans = sum(dp[n-1]) % MOD
 print(dp)
     
