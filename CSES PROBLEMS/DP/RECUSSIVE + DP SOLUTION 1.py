31beeb0ef41fc2aeb5f000330021514c77904eaa
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-10-09 10:45:19 -0400

    Daily push

---
+++ CSES PROBLEMS/DP/RECUSSIVE + DP SOLUTION 1.py
@@ -0,0 +1,70 @@
+
+
+n = int(input())
+
+vals = []
+for i in range(n):
+    vals.append(int(input()))
+
+def find_sums(value):
+    
+    summ = 0
+    if value <= 9:
+
+        print(sum(range(value + 1)))
+        return
+    
+    dp = [0] * (value + 1)
+    for i in range(10):
+        dp[i] = i
+        summ += i
+        
+    for i in range(10,value+1):
+        
+        for ch in str(i):
+            digit = int(ch)
+            dp[i] += dp[digit]
+        summ += dp[i]   
+    print(summ)
+    return
+
+for element in vals:
+    find_sums(element)
+        
+
+
+
+# Recussion: 
+
+def sum_digits(num):
+   
+    total = 0
+    while num > 0:
+        total += num % 10   
+        num //= 10        
+    return total
+
+
+
+def sum_all_digits(n):
+    
+    total = 0
+    def dfs(i, running_sum):
+        nonlocal total
+
+        # base case
+        if i > n:
+            total = running_sum
+            return
+
+        new_sum = running_sum + sum_digits(i)
+
+        dfs(i + 1, new_sum)
+
+    # start recursion
+    dfs(1, 0)
+    return total
+
+
+n = int(input("Enter n: "))
+print(sum_all_digits(n))
