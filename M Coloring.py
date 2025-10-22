2c449dd0e11338fd6019736b22b56d08b4fadb0a
Ousman Bah <142653505+Ousmanbah10@users.noreply.github.com>
2025-10-22 14:38:44 -0400

    Add sortColors method to sort colors in-place
    Implement in-place sorting of colors in the array.

--- M Coloring.py
+++ M Coloring.py
@@ -1,1 +1,26 @@
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        l ,r, m = 0,0, len(nums)-1
+
+        while m >= 0 and nums[m] == 2:
+            m -= 1
+        
+        while l <= m:
+
+            if nums[l] == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r += 1
+            elif nums[l] == 1:
+                l += 1
+               
+            elif nums[l] == 2:
+                nums[l], nums[m] = nums[m], nums[l]
+                m -= 1
+
+        return nums
