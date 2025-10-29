847cc667848ea920bfc25e8d46509f7783f8e9d1
Ousman Bah <142653505+Ousmanbah10@users.noreply.github.com>
2025-10-29 00:22:40 -0400

    Update Longest Substring without repeating Character.py

--- Sliding Window/Longest Substring without repeating Character.py
+++ Sliding Window/Longest Substring without repeating Character.py
@@ -1,1 +1,19 @@
 
+
+def longestNonRepeatingSubstring(self, s):
+        #your code goes here
+        maxx = 0
+        seen = set()
+        l = 0
+        for i in range(len(s)):
+            if s[i] not in seen:
+                seen.add(s[i])
+                maxx = max(maxx, i - l + 1)  
+            else:
+                while s[i] in seen:
+                    seen.remove(s[l])
+                    l += 1
+                seen.add(s[i])
+                maxx = max(maxx, i - l + 1)  
+           
+        return maxx
